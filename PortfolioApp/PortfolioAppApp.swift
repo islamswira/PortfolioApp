a28e21dcf45c855c881d0cd26ad31cb970445367
IslamSWIRA <islam.swira@flexxpay.com>
2022-10-22 15:33:46 +0300

    Initial Commit

---
+++ PortfolioApp/PortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAppApp.swift
+//  PortfolioApp
+//
+//  Created by IslamSwira on 22/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
